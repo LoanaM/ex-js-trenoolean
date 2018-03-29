65560a556a62d25b73d14e2ca2f96380b26268b5
LoanaWitch <loanawitch@live.com>
2018-03-29 23:36:14 +0200

    conversione orario fatta

--- trenoo.js
+++ trenoo.js
@@ -29,6 +29,7 @@ for (var i = 0; i < 10; i++) {
     train.destinazione = "Firenze"
     train.durata = Math.floor(Math.random() * (190 - 75)) + 75;
     train.orario = time();
+    train.ora =  oraminuti(clock);
     train.postiliberi =  Math.floor(Math.random() * 41);
     trains.push(train)
 }
@@ -41,6 +42,7 @@ for (var i = 0; i < 10; i++) {
     train.destinazione = "Roma"
     train.durata = Math.floor(Math.random() * (215 - 170)) + 170;
     train.orario = time();
+    train.ora = oraminuti(train.orario);
     train.postiliberi =  Math.floor(Math.random() * 41);
     trains.push(train)
 }
@@ -78,18 +80,31 @@ for (var i = 0; i < trainfi.length; i++) {
 }
 console.log(trenovuoto)
 
+// Cerco quello che parte prima
+var oraprecedente = trainfi[0].ora
+var trenoprecedente = trainfi[0]
 
-var partenza = prompt("Inserisci stazione di partenza");
-var destinazione = prompt("Inserisci stazione di arrivo");
-
-var trainut = []
-for (var i = 0; i < trains.length; i++) {
-    if ((trains[i].partenza == partenza) && (trains[i].destinazione == destinazione)) {
-        trainfi.push(trains[i])
-    }
+for (var i = 0; i < trainfi.length; i++) {
+  if (trainfi[i].ora < oraprecedente) {
+      trainfi[i].ora = oraprecedente;
+      trenoprecedente = trainfi[i]
+  }
 }
+console.log(trenoprecedente)
 
-console.log(trainut)
+
+
+// var partenza = prompt("Inserisci stazione di partenza");
+// var destinazione = prompt("Inserisci stazione di arrivo");
+//
+// var trainut = []
+// for (var i = 0; i < trains.length; i++) {
+//     if ((trains[i].partenza == partenza) && (trains[i].destinazione == destinazione)) {
+//         trainfi.push(trains[i])
+//     }
+// }
+//
+// console.log(trainut)
 
 
 // var ricerca = prompt("Selezionare: tempo-posti-orario")
@@ -113,7 +128,7 @@ console.log(trainut)
 
 
 
-// genero cod numerico di 3cifre
+// genero cod numerico di 4cifre
 function codegenerator () {
       unicode = [];
       for (var j = 0; j < 4; j++) {
@@ -125,13 +140,24 @@ function codegenerator () {
 
 // genero un orario
 function time () {
-  clock = [];
+  clock = "";
+  // ora = [];
   h = Math.floor(Math.random() * 24);
-  clock.push(h + ":")
   min = Math.floor(Math.random() * 60);
   if (min<10) {
     min = "0" + min
   }
-  clock.push(min)
-  return clock.join("")
+  clock = h + ":" + min
+  // ora.push(h)
+  // ora.push(min)
+  // oraunita = ora.join("")
+
+  return clock
+}
+
+// trasforma le ore in minuti
+function oraminuti (orologio) {
+    ora = orologio.split(":");
+    minuti = ora[0] * 60 + parseInt(ora[1]);
+    return minuti
 }
