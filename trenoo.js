750d0055565fcf97c44c36c65eacebeb949a758c
LoanaWitch <loanawitch@live.com>
2018-03-29 23:48:24 +0200

    ricerca x durata ok

--- trenoo.js
+++ trenoo.js
@@ -42,7 +42,7 @@ for (var i = 0; i < 10; i++) {
     train.destinazione = "Roma"
     train.durata = Math.floor(Math.random() * (215 - 170)) + 170;
     train.orario = time();
-    train.ora = oraminuti(train.orario);
+    train.ora = oraminuti(clock);
     train.postiliberi =  Math.floor(Math.random() * 41);
     trains.push(train)
 }
@@ -54,7 +54,7 @@ for (var i = 0; i < trains.length; i++) {
         trainfi.push(trains[i])
     }
 }
-console.log(trainfi)
+// console.log(trainfi)
 
 //cerco il più veloce
 var veloce = trainfi[0].durata
@@ -94,37 +94,36 @@ console.log(trenoprecedente)
 
 
 
-// var partenza = prompt("Inserisci stazione di partenza");
-// var destinazione = prompt("Inserisci stazione di arrivo");
-//
-// var trainut = []
-// for (var i = 0; i < trains.length; i++) {
-//     if ((trains[i].partenza == partenza) && (trains[i].destinazione == destinazione)) {
-//         trainfi.push(trains[i])
-//     }
-// }
-//
-// console.log(trainut)
+var partenza = prompt("Inserisci stazione di partenza");
+var destinazione = prompt("Inserisci stazione di arrivo");
+
+var trainut = []
+for (var i = 0; i < trains.length; i++) {
+    if ((trains[i].partenza == partenza) && (trains[i].destinazione == destinazione)) {
+        trainut.push(trains[i])
+    }
+}
+console.log(trainut)
 
 
-// var ricerca = prompt("Selezionare: tempo-posti-orario")
-//
-// if (ricerca=="tempo") {
-//   var veloceut = trainut[0].durata
-//   var trenoutvel = trainut[0]
-//   for (var i = 0; i < trainut.length; i++) {
-//     if (trainut[i].durata < veloceut) {
-//         trainut[i].durata = veloceut
-//         trenoutvel = trainut[i]
-//     }
-//   }
-//   console.log(trenoutvel)
-// }
+var ricerca = prompt("Selezionare: tempo-posti-orario")
 
-// if (trenoveloce.postiliberi>0) {
-//   trenoveloce.postiliberi--
-//   console.log (codegenerator())
-// }
+if (ricerca=="tempo") {
+  var veloceut = trainut[0].durata
+  var trenoutvel = trainut[0]
+  for (var i = 0; i < trainut.length; i++) {
+    if (trainut[i].durata < veloceut) {
+        trainut[i].durata = veloceut
+        trenoutvel = trainut[i]
+    }
+  }
+  console.log(trenoutvel)
+}
+
+if (trenoveloce.postiliberi>0) {
+  trenoveloce.postiliberi--
+  console.log (codegenerator())
+}
 
 
 
@@ -140,19 +139,14 @@ function codegenerator () {
 
 // genero un orario
 function time () {
-  clock = "";
-  // ora = [];
-  h = Math.floor(Math.random() * 24);
-  min = Math.floor(Math.random() * 60);
-  if (min<10) {
-    min = "0" + min
-  }
-  clock = h + ":" + min
-  // ora.push(h)
-  // ora.push(min)
-  // oraunita = ora.join("")
-
-  return clock
+    clock = "";
+    h = Math.floor(Math.random() * 24);
+    min = Math.floor(Math.random() * 60);
+    if (min<10) {
+      min = "0" + min
+    }
+    clock = h + ":" + min
+    return clock
 }
 
 // trasforma le ore in minuti
